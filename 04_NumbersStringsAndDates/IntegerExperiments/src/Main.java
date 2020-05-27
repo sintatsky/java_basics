2e1a1b0502a3749a6be6506decc63cd23c77551e
Sintatski <kreslovi@bk.ru>
2020-05-27 18:41:41 +0300

    Homework 4.1

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,21 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+
+public class Main {
+
+    public static void main(String[] args) {
         Container container = new Container();
         container.count += 7843;
 
+
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
-        return 0;
+    public static Integer sumDigits(int number) {
+        String str = Integer.toString(number);
+        int a = 0;
+        for (int i = 0; i < str.length(); i++)
+            a = a + Integer.parseInt(str.charAt(i) + "");
+
+        return a;
+
     }
+
 }
