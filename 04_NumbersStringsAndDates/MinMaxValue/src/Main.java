ca51264c3da56d820980b00b1d571a1e6cbb7fea
Sintatski <kreslovi@bk.ru>
2020-07-22 13:16:10 +0300

    4.2HW

---
+++ 04_NumbersStringsAndDates/MinMaxValue/src/Main.java
@@ -0,0 +1,44 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        byte by_min = Byte.MIN_VALUE;
+        byte by_max = Byte.MAX_VALUE;
+
+        int i_min = Integer.MIN_VALUE;
+        int i_max = Integer.MAX_VALUE;
+
+        short sh_min = Short.MIN_VALUE;
+        short sh_max = Short.MAX_VALUE;
+
+        long lg_min = Long.MIN_VALUE;
+        long lg_max = Long.MAX_VALUE;
+
+        float fl_min = Float.MIN_VALUE;
+        float fl_max = Float.MAX_VALUE;
+
+        double dl_min = Double.MIN_VALUE;
+        double dl_max = Double.MAX_VALUE;
+
+        System.out.println("Минимальное отричательное byte " + by_min);
+        System.out.println("Максимальное положительное byte " + by_max);
+
+        System.out.println("Минимальное отричательное  int " + i_min);
+        System.out.println("Максимальное положительное int " + i_max);
+
+        System.out.println("Минимальное отричательное short " + sh_min);
+        System.out.println("Максимальное положительное short " + sh_max);
+
+        System.out.println("Минимальное отричательное  long " + lg_min);
+        System.out.println("Максимальное положительное long " + lg_max);
+
+        System.out.println("Минимальное отричательное float " + fl_min);
+        System.out.println("Максимальное положительное float " + fl_max);
+
+        System.out.println("Минимальное отричательное  double " + dl_min);
+        System.out.println("Максимальное положительное double " + dl_max);
+
+    }
+}
