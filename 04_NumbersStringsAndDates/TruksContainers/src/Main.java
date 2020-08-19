e8da4d5fb3c266506d544de41c3a05d75ef469b4
Sintatski <kreslovi@bk.ru>
2020-08-19 11:44:28 +0300

    4.1 done

--- 04_NumbersStringsAndDates/TruksContainers/src/Main.java
+++ 04_NumbersStringsAndDates/TruksContainers/src/Main.java
@@ -2,61 +2,38 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-
 public class Main {
   public static void main(String[] args) {
-      int container = 0;
-      try (Scanner scanner = new Scanner(System.in)) {
-          System.out.println("How many containers you have?");
-          container = scanner.nextInt();
-      } catch (Exception e) {
-          e.printStackTrace();
+
+    final int BOX_IN_CONTAINER = 27;
+    final int CONTAINER_IN_TRUCK = 12;
+    final int BOX_IN_TRUCK = BOX_IN_CONTAINER * CONTAINER_IN_TRUCK;
+
+    Scanner scanner = new Scanner(System.in);
+    for (; ; ){
+      System.out.println(" == Введите число ящиков :");
+      if (scanner.hasNextInt()) {
+        int boxCount = scanner.nextInt();
+
+        int containerCount = 0;
+        int truckCount = 0;
+
+        int i = 0;
+        while (i < boxCount) {
+          if (i % BOX_IN_TRUCK == 0) {
+            System.out.println("Грузовик " + ++truckCount);
+          }
+          if (i % BOX_IN_CONTAINER == 0) {
+            System.out.println("\tКонтейнер " + ++containerCount);
+          }
+          System.out.println("\t\tЯщик " + ++i);
+        }
+        System.out.printf(
+            "Необходимо Грузовиков:%d, Контейнеров:%d %n", truckCount, containerCount);
+      } else {
+        System.out.println("Ошибка.Введите число повторно");
+        scanner.nextLine(); // отчистка сканера
       }
-      Cargo cargo = new Cargo(container);
-      System.out.println(cargo);
-      cargo.result();
-
+    }
   }
-}
-
-class Cargo{
-    private int truck;
-    private int container;
-    private int box;
-    final int CONTAINERS_IN_TRUCK = 12;
-    final int BOXES_IN_CONTAINER = 27;
-
-    public Cargo(int box){
-        this.box = box;
-       if (box != 0) {
-            container = box % BOXES_IN_CONTAINER == 0 ? box / BOXES_IN_CONTAINER : box / BOXES_IN_CONTAINER + 1;
-        }
-        if (container != 0){
-            truck = container % CONTAINERS_IN_TRUCK == 0 ? container / CONTAINERS_IN_TRUCK : container / CONTAINERS_IN_TRUCK + 1;
-        }
     }
-    public int getTruck(){
-        return truck;
-    }
-    public int getContainer(){
-        return container;
-    }
-    public int getBox(){
-        return box;
-    }
-    public void result(){
-        int j=1;
-        int i;
-        for (i = 1; i <= container; i++){
-
-           if (i % 27 == 0 && j % 12 == 0){
-               container++;
-               truck++;
-               System.out.println("Контейнер " + container);
-               System.out.println("Грузовиков " + truck);
-
-            }
-        }
-    }
-    }
-
