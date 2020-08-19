726d6c62d6dc6a8036196c7353f9caa072a9e082
Sintatski <kreslovi@bk.ru>
2020-08-19 16:08:59 +0300

    4.4.2 done

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,21 +1,19 @@
-
 public class Loader
 {
     public static void main(String[] args)
     {
-    String text = "Вася заработал 5000\u20BD,\tПетя - 7563\u20BD, \tа Маша - 30000\u20BD";
-    String vasyaSalary = text.substring(15, 19);
-    int spaceLastIndex = text.lastIndexOf(' ');
-    String mashaSalary = text.substring(51, spaceLastIndex);
-    int i = Integer.parseInt(vasyaSalary);
-    int k = Integer.parseInt(mashaSalary);
-    try{
-       int result = Integer.parseInt(mashaSalary);
-        System.out.println(result);
-    } catch (NumberFormatException e) {
-        System.err.println("Unable to convert input string :" + mashaSalary + " to int");
-    }
-    int amountSalary = i + k;
-    System.out.println(amountSalary);
+    String text = "Вася заработал 5000\u20bd, Петя - 7563\u20bd, а Маша - 30000\u20bd";
+
+        System.out.println(text);
+        int v1 = text.indexOf("Вася заработал");
+        int v2 = text.indexOf("\u20bd");
+        String str1 = text.substring(v1 + 14, v2).trim();
+
+        int m1 = text.indexOf("Маша - ");
+        int m2 = text.lastIndexOf("\u20bd");
+        String str2 = text.substring(m1 + 6, m2).trim();
+
+        int amountMoney = Integer.parseInt(str1)+Integer.parseInt(str2);
+        System.out.println("Сумма заработка Васи и Маши равна " + amountMoney + " \u20bd");
     }
 }
