4c92c99217777dbad5acf1e61758302eac2be1f4
Sintatski <kreslovi@bk.ru>
2020-08-19 15:07:00 +0300

    4.4.1 done

--- 04_NumbersStringsAndDates/Code/src/Code.java
+++ 04_NumbersStringsAndDates/Code/src/Code.java
@@ -1,83 +1,9 @@
 public class Code {
   public static void main(String[] args) {
-   // String text  = "Какой-то текст";
-    char a = 'A';
-    char b = 'B';
-    char c = 'C';
-    char d = 'D';
-    char e = 'E';
-    char f = 'F';
-    char g = 'G';
-    char h = 'H';
-    char i = 'I';
-    char j = 'J';
-    char k = 'K';
-    char l = 'L';
-    char m = 'M';
-    char n = 'N';
-    char o = 'O';
-    char p = 'P';
-    char q = 'Q';
-    char r = 'R';
-    char s = 'S';
-    char t = 'T';
-    char u = 'U';
-    char v = 'V';
-    char w = 'W';
-    char x = 'X';
-    char y = 'Y';
-    char z = 'Z';
+    for (char i = 'A'; i <= 'Z'; i++){
+        int code = i;
+      System.out.println(i +":"+  code);
+    }
 
-    int code1 = (int) a;
-    int code2 = (int) b;
-    int code3 = (int) c;
-    int code4 = (int) d;
-    int code5 = (int) e;
-    int code6 = (int) f;
-    int code7 = (int) g;
-    int code8 = (int) h;
-    int code9 = (int) i;
-    int code10 = (int) j;
-    int code11 = (int) k;
-    int code12 = (int) l;
-    int code13 = (int) m;
-    int code14 = (int) n;
-    int code15 = (int) o;
-    int code16 = (int) p;
-    int code17 = (int) q;
-    int code18 = (int) r;
-    int code19 = (int) s;
-    int code20 = (int) t;
-    int code21 = (int) v;
-    int code22 = (int) w;
-    int code23 = (int) x;
-    int code24 = (int) y;
-    int code25 = (int) z;
-
-    System.out.println( "A - " + code1);
-    System.out.println("B - " + code2);
-    System.out.println("C - " + code3);
-    System.out.println("D - " + code4);
-    System.out.println("E - " + code5);
-    System.out.println( "F - " + code6);
-    System.out.println("G - " + code7);
-    System.out.println("H - " + code8);
-    System.out.println("I - " + code9);
-    System.out.println("J - " + code10);
-    System.out.println( "K - " + code11);
-    System.out.println("L - " + code12);
-    System.out.println("M - " + code13);
-    System.out.println("N - " + code14);
-    System.out.println("O - " + code15);
-    System.out.println( "P - " + code16);
-    System.out.println("Q - " + code17);
-    System.out.println("R - " + code18);
-    System.out.println("S - " + code19);
-    System.out.println("T - " + code20);
-    System.out.println( "V - " + code21);
-    System.out.println("W - " + code22);
-    System.out.println("X - " + code23);
-    System.out.println("Y - " + code24);
-    System.out.println("Z - " + code25);
   }
 }
--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -3,8 +3,19 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-
-        System.out.println(text);
+    String text = "Вася заработал 5000\u20BD,\tПетя - 7563\u20BD, \tа Маша - 30000\u20BD";
+    String vasyaSalary = text.substring(15, 19);
+    int spaceLastIndex = text.lastIndexOf(' ');
+    String mashaSalary = text.substring(51, spaceLastIndex);
+    int i = Integer.parseInt(vasyaSalary);
+    int k = Integer.parseInt(mashaSalary);
+    try{
+       int result = Integer.parseInt(mashaSalary);
+        System.out.println(result);
+    } catch (NumberFormatException e) {
+        System.err.println("Unable to convert input string :" + mashaSalary + " to int");
+    }
+    int amountSalary = i + k;
+    System.out.println(amountSalary);
     }
 }
