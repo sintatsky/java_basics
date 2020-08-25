d4123e39190f24fa1c250abe531e154e1a052c28
Sintatski <kreslovi@bk.ru>
2020-08-25 17:26:35 +0300

    4.4 " * " исправлено

--- 04_NumbersStringsAndDates/Safe/src/Main.java
+++ 04_NumbersStringsAndDates/Safe/src/Main.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите номер кредитной карты");
         String text = scanner.nextLine();
-        System.out.println(text.replaceAll("<4008 1234 5678 8912", "***"));
-        System.out.println(searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***"));
-    }
+        String Str = new String("4008 1234 5678 8912");
+        System.out.println(text.replaceAll("4008 1234 5678", "Номер кредитной карты ***"));
+
+
+        }
 
   public static String searchAndReplaceDiamonds(String text, String placeholder) {
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
 
-    placeholder = "***";
-    String safe;
-      safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678 8912", placeholder);
-      String var = " ";
-    for (int i = 0; i < text.length(); i++) {
-      if (Character.isDigit(text.trim().charAt(i))) {
-        var = text.substring(i);
-      }
+    return safe;
+
     }
-return safe;
-    }
+
 }
+
+
+
