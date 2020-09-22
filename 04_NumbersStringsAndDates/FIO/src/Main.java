8228b42c76afdf18954b3c1454bfc7209f3d3ca0
Sintatski <kreslovi@bk.ru>
2020-09-22 06:21:59 +0300

    Домашняя работа 4.5.3

--- 04_NumbersStringsAndDates/FIO/src/Main.java
+++ 04_NumbersStringsAndDates/FIO/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
   public static void main(String[] args) {
       String name = "Иванов Сергей Петрович";
-      int spaceIndex = name.indexOf(' ');
-      int spaceIndex2 = name.lastIndexOf(' ');
-      System.out.println("Фамилия:\t " + name.substring(0, spaceIndex) + "\nИмя:\t\t " + name.substring(spaceIndex, spaceIndex2) + "\nОтчество:\t " + name.substring(spaceIndex2));
+      String[] name1 = name.split("\\s");
+      for (int i = 0; i < name1.length; i++){
+      System.out.println(name1[i]);
+      }
 
   }
 }
