5e029e7ba368656f8239ded5af8290da89d3a32c
Sintatski <kreslovi@bk.ru>
2020-09-22 05:44:33 +0300

    Домашняя работа 4.5.1

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,20 +1,16 @@
-public class Loader
-{
-    public static void main(String[] args)
+import java.util.Arrays;
+
+public class Loader {
+  public static void main(String[] args) {
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+      String[] money = text.split(" ");
+      for (int i = 0; i < money.length; i++) ;
     {
-        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-
-        String money = text.replaceAll("[^0-9]", "");
-        String str1 = money.substring(0,4);
-        String str2 = money.substring(4,8);
-        String str3 = money.substring(8,13);
-
-        int amountMoney = Integer.parseInt(str1) + Integer.parseInt(str2) + Integer.parseInt(str3);
-
-        System.out.println("Вася заработал " + str1+"\u20bd,"+" Петя - "+str2+"\u20bd,"+" а Маша - "+str3+"\u20bd");
-        System.out.println("Общая сумма их заработка " + amountMoney);
-
-
-
+        System.out.println("Вася заработал " + money[2]+"\u20bd," +" Петя - "+ money[6]+"\u20bd,"+" а Маша - "+money[11]+"\u20bd");
     }
-}
+        int amountMoney = Integer.parseInt(money[2]) + Integer.parseInt(money[6]) + Integer.parseInt(money[11]);
+        System.out.println("Общая сумма их заработка " + amountMoney);
+    }
+  }
+
