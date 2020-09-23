79b60670dac3eae01016f92ae722d1cf99a0a603
Sintatski <kreslovi@bk.ru>
2020-09-23 16:55:29 +0300

    Домашняя работа 4.5.1

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -3,8 +3,18 @@ import java.util.Arrays;
 public class Loader {
   public static void main(String[] args) {
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-
-    String[] money = text.split(" ");
+    String money = text.replaceAll("[^0-9]"," ").trim();
+    int spaceIndex = money.indexOf(' ');
+    int spaceIndex2 = money.lastIndexOf(' ');
+    String vasya = money.substring(0, spaceIndex);
+    String petya = money.substring(spaceIndex, spaceIndex2).trim();
+    String masha = money.substring(spaceIndex2+1);
+    int vasyaMoney = Integer.parseInt(vasya);
+    int petyaMoney = Integer.parseInt(petya);
+    int mashaMoney = Integer.parseInt(masha);
+    int moneyAmount = vasyaMoney + petyaMoney + mashaMoney;
+    System.out.println("Вася заработал " + vasyaMoney + ", Петя - " + petyaMoney + ", а Маша - " +mashaMoney);
+    System.out.println("Общая сумма их заработка " + moneyAmount);
 
 
      }
