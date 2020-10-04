9ae24cb88d9b77eb1012ad780c68f027ffecafd9
Sintatski <kreslovi@bk.ru>
2020-10-04 10:17:28 +0300

    Домашняя работа 4.6

--- 04_NumbersStringsAndDates/Calendar/src/Loader.java
+++ 04_NumbersStringsAndDates/Calendar/src/Loader.java
@@ -1,20 +1,28 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
+import java.util.Date;
 
 
 public class Loader {
   public static void main(String[] args) {
 
+      int day = 23;
+      int month = Calendar.FEBRUARY;
+      int year = 1989;
+
+      Date birthday = new Date(0);
+      Date today = new Date(31);
 
       DateFormat dateFormat = new SimpleDateFormat("EEEE d MMM y");
-
       Calendar calendar = Calendar.getInstance();
-      calendar.set(1988,1, 23);
+      calendar.set(year, month, day);
+      calendar.setTime(new Date());
+      int birthday1 = calendar.get(Calendar.DATE);
 
-      for (int i = 0; i <= 31; i++ ){
+       while (today.after(birthday)) {
 
+          birthday1 = birthday1 +1;
           calendar.add(calendar.YEAR, 1);
           System.out.println(dateFormat.format(calendar.getTime()));
       }
