0985a6e3d14e515e2753b90de4c66ab11e75c4de
Sintatski <kreslovi@bk.ru>
2020-10-05 07:00:25 +0300

    4.4 " *** "

--- 04_NumbersStringsAndDates/Safe/src/Main.java
+++ 04_NumbersStringsAndDates/Safe/src/Main.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите номер кредитной карты ");
+        System.out.println("Введите номер кредитной карты ");          //4008 1234 5678 8912
         String text = scanner.nextLine();
 
         String safe = "Номер кредитной карты <4008 1234 5678>  ";
-        int diamond1 = safe.indexOf('<');
-        int diamond2 = safe.indexOf('>');
-        String placeholder = "***";
-        System.out.println(safe.substring(0, diamond1) + placeholder + safe.substring(diamond2 + 1));
+        System.out.println(safe.replaceAll("<4008 1234 5678>", "***"));
+
+
     }
 }
