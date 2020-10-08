ca4c5326589571a1a2829a021a7d858bc5634831
Sintatski <kreslovi@bk.ru>
2020-10-08 03:14:27 +0300

    Домашняя работа 4.6

--- 04_NumbersStringsAndDates/Calendar/src/Loader.java
+++ 04_NumbersStringsAndDates/Calendar/src/Loader.java
@@ -2,12 +2,37 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 
 public class Loader {
   public static void main(String[] args) {
+      final String PRINT_DATE_FORMAT = "EE d.MM.yyyy ";
+      final Locale LOCALE = new Locale("ru");
+      SimpleDateFormat sdf = new SimpleDateFormat(PRINT_DATE_FORMAT, LOCALE);
+      Calendar today = Calendar.getInstance();
+      Calendar birthday = Calendar.getInstance();
+      birthday.set(1989, Calendar.FEBRUARY, 23);
 
-      DateFormat dateFormat = new SimpleDateFormat("EEEE d MMM y");
+
+      while (birthday.before(today)) {
+          Date date = birthday.getTime();
+          System.out.println(sdf.format(date));
+          birthday.add(Calendar.YEAR, 1);
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+    /*  DateFormat dateFormat = new SimpleDateFormat("EEEE d MMM y");
       Calendar calendar = Calendar.getInstance();
 
       int day = 23;
@@ -26,7 +51,7 @@ public class Loader {
               calendar.add(Calendar.YEAR, 1);
               System.out.println(dateFormat.format(calendar.getTime()));
       }
-
+*/
 
 
   }
