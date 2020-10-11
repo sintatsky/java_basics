b9dfa115475636b665bf0cfb72c1e4245863670e
Sintatski <kreslovi@bk.ru>
2020-10-11 07:58:14 +0300

    Домашняя работа 5.1 Дополнительное задание*

--- 05_ArraysAndCollections/DOX/src/Main.java
+++ 05_ArraysAndCollections/DOX/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        String[][] cross = {{"x      x",
+        String[][] cross = {{"x     x",
                 " x   x",
                 "  x x",
                 "   x",
                 "  x x",
                 " x   x",
-                "x      x"}};
+                "x     x"}};
 
         int i = 0;
         for (int j = 0; j < cross[i].length; j++) {
