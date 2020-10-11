00cdfa99dc16b247ba8ba66da9a4cd82aa43cf74
Sintatski <kreslovi@bk.ru>
2020-10-11 07:07:38 +0300

    Домашняя работа 5.1 Задание 2

--- 05_ArraysAndCollections/AverageTemperature/src/Main.java
+++ 05_ArraysAndCollections/AverageTemperature/src/Main.java
@@ -1,9 +1,10 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        float[] patientsTemperature = {
+
+        float[] patientsTemperature = new float[]{
                 (float) 40.0, (float) 34.2, (float) 39.8, (float) 36.4, (float) 36.6, (float) 36.9, (float) 35.5, (float) 37.8,
                 (float) 33.2, (float) 34.2, (float) 40.0, (float) 38.1, (float) 37.4, (float) 38.2, (float) 38.5, (float) 38.4, (float) 36.9,
                 (float) 37.1, (float) 38.1, (float) 33.2, (float) 32.3, (float) 35.3, (float) 37.2, (float) 34.7, (float) 34.5, (float) 36.5,
@@ -11,35 +12,36 @@ public class Main {
 
 
         final int NUMBER_OF_PATIENTS = 30;
-        final int MIM_TEMP = 32;
-        final int MAX_TEMP = 40;
         final float MIN_TEMP_OF_HEALTHY_PATIENT = (float) 36.2;
         final float MAX_TEMP_OF_HEALTHY_PATIENT = (float) 36.9;
 
 
-    for (float i =0; i < patientsTemperature.length; i++){
-        System.out.println("Температуры пациентов " + patientsTemperature[(int) i]);
-      }
+        // for (float patientTemp : patientsTemperature){
+
+        System.out.printf("Температуры пациентов " + Arrays.toString(patientsTemperature));
+        //   }
 
 
         float sum = 0;
         for (float num : patientsTemperature) {
             sum = sum + num;
         }
-        System.out.println("Средняя температура: " + sum / NUMBER_OF_PATIENTS);
+        System.out.println("\nСредняя температура: " + sum / NUMBER_OF_PATIENTS);
 
 
         int count = 0;
+
+
         for (int i = 0; i < patientsTemperature.length; i++) {
             if (patientsTemperature[i] > MIN_TEMP_OF_HEALTHY_PATIENT
                     && patientsTemperature[i] < MAX_TEMP_OF_HEALTHY_PATIENT) {
                 count++;
             }
+
             System.out.println("Количество здоровых:  " + count);
         }
 
     }
 }
-
 
 
