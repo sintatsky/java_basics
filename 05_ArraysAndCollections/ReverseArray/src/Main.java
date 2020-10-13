6bf0f31b5d9414d4e128c9a04e6c1c80b718c865
Sintatski <kreslovi@bk.ru>
2020-10-13 19:28:47 +0300

    Домашняя работа 5.1 Задание 1

--- 05_ArraysAndCollections/ReverseArray/src/Main.java
+++ 05_ArraysAndCollections/ReverseArray/src/Main.java
@@ -2,8 +2,20 @@ public class Main {
   public static void main(String[] args) {
     String[] colors = {"Каждый", "охотник", "желает", "знать", "где", "сидит", "фазан"};
 
-    for (int i = colors.length - 1; i >= 0; i--) {
+//    for (int i = 0; i < colors.length; i++){
+//      System.out.println(colors[i]);
+//    }
+    String temp;
+
+    for (int i = 0; i < colors.length / 2; i++) {
+      temp = colors[colors.length - i - 1];
+      colors[colors.length - i - 1] = colors[i];
+      colors[i] = temp;
+    }
+
+    for (int i = 0; i < colors.length; i++) {
       System.out.println(colors[i]);
     }
+
   }
 }
