65e4d3fd52ae8865901625f54b314bef47398dcc
Sintatski <kreslovi@bk.ru>
2020-10-13 20:24:25 +0300

    Домашняя работа 5.1 Задание 2

--- 05_ArraysAndCollections/AverageTemperature/src/Main.java
+++ 05_ArraysAndCollections/AverageTemperature/src/Main.java
@@ -1,44 +1,40 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        float[] patientsTemperature = new float[]{
-                (float) 40.0, (float) 34.2, (float) 39.8, (float) 36.4, (float) 36.6, (float) 36.9, (float) 35.5, (float) 37.8,
-                (float) 33.2, (float) 34.2, (float) 40.0, (float) 38.1, (float) 37.4, (float) 38.2, (float) 38.5, (float) 38.4, (float) 36.9,
-                (float) 37.1, (float) 38.1, (float) 33.2, (float) 32.3, (float) 35.3, (float) 37.2, (float) 34.7, (float) 34.5, (float) 36.5,
-                (float) 39.4, (float) 39.8, (float) 38.1, (float) 38.2};
-
+        float[] patientsTemperature = new float[30];
 
         final int NUMBER_OF_PATIENTS = 30;
+        final float MIN_TEMP = 32;
+        final float MAX_TEMP = 40;
         final float MIN_TEMP_OF_HEALTHY_PATIENT = (float) 36.2;
         final float MAX_TEMP_OF_HEALTHY_PATIENT = (float) 36.9;
 
 
-        // for (float patientTemp : patientsTemperature){
+        for (float i = 0; i < patientsTemperature.length; i++) {
+            patientsTemperature[(int) i] = ((float) (Math.random() * (MAX_TEMP - MIN_TEMP) + MIN_TEMP));
 
-        System.out.printf("Температуры пациентов " + Arrays.toString(patientsTemperature));
-        //   }
+            System.out.println("Температура пациента:\t " + String.format("%.1f",patientsTemperature[(int) i]));
+        }
 
 
         float sum = 0;
         for (float num : patientsTemperature) {
             sum = sum + num;
         }
-        System.out.println("\nСредняя температура: " + sum / NUMBER_OF_PATIENTS);
+        System.out.println("\nСредняя температура: \t" + String.format("%.1f" , (sum / NUMBER_OF_PATIENTS)) + "\n" );
 
-
-        int count = 0;
 
 
         for (int i = 0; i < patientsTemperature.length; i++) {
+            int count = 0;
             if (patientsTemperature[i] > MIN_TEMP_OF_HEALTHY_PATIENT
                     && patientsTemperature[i] < MAX_TEMP_OF_HEALTHY_PATIENT) {
                 count++;
             }
 
-            System.out.println("Количество здоровых:  " + count);
+            System.out.println("Количество здоровых:  \t" + count);
         }
 
     }
