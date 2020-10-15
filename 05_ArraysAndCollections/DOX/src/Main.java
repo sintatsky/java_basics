37b6d84953b450b43046d11aef65ad8bd1eb14ab
Sintatski <kreslovi@bk.ru>
2020-10-15 11:13:05 +0300

    Домашняя работа 5.1 Дополнительное задание*

--- 05_ArraysAndCollections/DOX/src/Main.java
+++ 05_ArraysAndCollections/DOX/src/Main.java
@@ -1,17 +1,40 @@
+
+
 public class Main {
     public static void main(String[] args) {
+        String[][] cross = new String[3][3];
+        for(int i = 0; i < cross.length; i++){
+            for(int k = 0; k <= i; k++){
+                System.out.println(" ");
+            }
+            for (int j = cross.length - 1; j > i; j--){
+                if (j == cross.length - i  - 1 || j == i + 1)
+                    System.out.println("x");
+                else
+                    System.out.println("  ");
 
-        String[][] cross = {{"x     x",
-                " x   x",
-                "  x x",
-                "   x",
-                "  x x",
-                " x   x",
-                "x     x"}};
-
-        int i = 0;
-        for (int j = 0; j < cross[i].length; j++) {
-            System.out.println(cross[i][j]);
+            }
+            System.out.println();
         }
-    }
+        for (int i = 0; i < cross.length; i++ ){
+            for (int v = cross.length; v > i; v --){
+                System.out.println(" ");
+            }
+            for (int j = 0; j < i; j++){
+                for (int h = 0; h <= j; h++)
+                System.out.println("x");
+            }
+        }
+      }
 }
+
+
+
+
+
+
+
+
+
+
+
