b2a32edb416e321c4ff4c00df01d01a06da20ad3
Sintatski <kreslovi@bk.ru>
2020-10-21 16:02:29 +0300

    Домашняя работа 5.2

--- 05_ArraysAndCollections/Lislts/src/Main.java
+++ 05_ArraysAndCollections/Lislts/src/Main.java
@@ -1,56 +1,93 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.IOException;
+
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
-        String deals;
-        String nameOfDeal;
-        int dealNumber;
-        String editName;
-        int deleteDeal;
-        System.out.println("Выберите действие и введите одну из комманд:  LIST  ADD  EDIT  DELETE");
+    ArrayList<String> todoList = new ArrayList<>() {{
+      add(0, "Встать поутру");
+      add(1, "Умыться");
+      add(2, "Привести себя в порядок");
+      add(3, "Привести планету в порядок");
+      add(4, "Выполоть баобабы");
+    }};
 
-        Scanner scanner = new Scanner(System.in);
-        deals = scanner.nextLine();
-        ArrayList<String> toDoList = new ArrayList<>() {{
-            add(0, "Встать поутру");
-            add(1, "Умыться");
-            add(2, "Привести себя в порядок");
-            add(3, "Привести планету в порядок");
-            add(4, "Выполоть баобабы");
-        }};
+    String nameOfDeal;
+    int dealNumber ;
+    String editName;
+    int deleteDeal;
 
 
-        if ("ADD".equals(deals)) {
-            System.out.println("Введите, какое дело вы хотите добавить ");
-            Scanner scanner1 = new Scanner(System.in);
-            nameOfDeal = scanner1.nextLine();
-            toDoList.add(nameOfDeal);
-            System.out.println(toDoList);
+    System.out.println("ДОБРО ПОЖАЛОВАТЬ. ВЫБЕРИТЕ ДЕЙСТВИЕ:  \n\nLIST - вывести список дел на экран  " +
+            "\nADD- пополнить список дел" +
+            "\nEDIT - заменить дело с указанным номером; " +
+            "\nDELETE - удалить дело из списка " +
+            "\nEXIT - выход");
 
-        } else if ("EDIT".equals(deals)) {
-            System.out.println("Введите порядковый номер дела, которое вы хотите изменить");
-            Scanner scanner2 = new Scanner(System.in);
-            dealNumber = scanner2.nextInt();
-            toDoList.remove(dealNumber);
-            System.out.println("Введите, новое задание вместо старого");
-            Scanner scanner3 = new Scanner(System.in);
-            editName = scanner3.nextLine();
-            toDoList.add(dealNumber, editName);
-            System.out.println(toDoList);
+    System.out.println("\nВВОД: ");
 
-        } else if ("DELETE".equals(deals)) {
-            System.out.println("Введите порядковый номер дела, которое вы хотите удалить");
-            Scanner scanner4 = new Scanner(System.in);
-            deleteDeal = scanner4.nextInt();
-            toDoList.remove(deleteDeal);
-            System.out.println(toDoList);
-        }
-        if ("LIST".equals(deals)) {
-            for (String s : toDoList) {
-                System.out.println(s);
-            }
-        }
+
+    while (true) {
+      Scanner sc = new Scanner(System.in);
+      switch (sc.nextLine()) {
+        case "LIST":
+          System.out.println("СПИСОК ДЕЛ: ");
+          for (String s : todoList) {
+            System.out.println(todoList.indexOf(s) +" "+ s);
+          }
+
+          continue;
+
+        case "ADD":
+          System.out.println("Введите, какое дело вы хотите добавить ");
+          Scanner scanner1 = new Scanner(System.in);
+          nameOfDeal = scanner1.next();
+          todoList.add(nameOfDeal);
+          System.out.println("Дело добавленно и находится в списке под №: " + todoList.size());
+          System.out.println();
+          for (String s : todoList) {
+            System.out.println(todoList.indexOf(s) +" "+ s);
+          }
+
+          continue;
+
+        case "DELETE":
+          System.out.println("Введите индекс дела, которое вы хотите удалить");
+          Scanner scanner4 = new Scanner(System.in);
+          deleteDeal = scanner4.nextInt();
+          todoList.remove(deleteDeal);
+          for (String s : todoList) {
+            System.out.println(todoList.indexOf(s) +" "+ s);
+          }
+          continue;
+
+        case "EDIT":
+
+          System.out.println("Введите порядковый номер дела, которое вы хотите изменить");
+          Scanner scanner2 = new Scanner(System.in);
+          dealNumber = scanner2.nextInt();
+          todoList.remove(dealNumber);
+          System.out.println("Введите, новое задание вместо старого");
+          Scanner scanner3 = new Scanner(System.in);
+          editName = scanner3.nextLine();
+          todoList.add(dealNumber, editName);
+          for (String s : todoList) {
+            System.out.println(todoList.indexOf(s) +" "+ s);
+          }
+          continue;
+
+        default:
+          System.out.println("Попробуйте еще раз");
+          break;
+
+        case "EXIT":
+          System.out.println("ДО НОВЫХ ВСТРЕЧ!");
+          break;
+      }
+      sc.close();
     }
+
+  }
 }
