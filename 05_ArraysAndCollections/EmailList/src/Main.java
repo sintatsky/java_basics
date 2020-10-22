d3546d363e3081ef2e4633f3421aaa841d6b6152
Sintatski <kreslovi@bk.ru>
2020-10-22 14:25:25 +0300

    Домашняя работа 5.3

--- 05_ArraysAndCollections/EmailList/src/Main.java
+++ 05_ArraysAndCollections/EmailList/src/Main.java
@@ -1,14 +1,16 @@
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {
-    public static void main(String[] args) {
-        String email;
+    public static void main(String[] args) throws IOException {
         String addedEmail;
 
-        System.out.println("Выберите действие и введите одну из комманд:  LIST  ADD  ");
-        Scanner scanner = new Scanner(System.in);
-        email = scanner.nextLine();
+        System.out.println("Выберите действие и введите одну из комманд:  " +
+                "\nLIST - вывести на экран список электронных адресов " +
+                "\nADD - добавить электронный адрес  " +
+                "\nEXIT - выход");
+
         TreeSet<String> emails = new TreeSet<>() {{
             add("hello@skillbox.ru");
             add("allday@skillbox.ru");
@@ -16,7 +18,12 @@ public class Main {
             add("lertur@skillbox.ru");
             add("support@skillbox.ru");
         }};
-        if ("ADD".equals(email)) {
+
+        while (true){
+            Scanner sc = new Scanner(System.in);
+            switch (sc.nextLine()){
+
+                case  "ADD":
             System.out.println("Введите адрес, который  вы хотите добавить ");
             Scanner scanner1 = new Scanner(System.in);
             addedEmail = scanner1.nextLine();
@@ -26,12 +33,18 @@ public class Main {
             else {
                 System.out.println("Адрес введен неверно. Повторите попытку");
             }
-        }
+            continue;
 
-        if ("LIST".equals(email)) {
+            case "LIST":
             for (String s : emails) {
                 System.out.println(s);
             }
+            continue;
+                case "EXIT":
+                    System.out.println("Хорошего дня!");
+                    break;
+            }
+            sc.close();
         }
 
     }
